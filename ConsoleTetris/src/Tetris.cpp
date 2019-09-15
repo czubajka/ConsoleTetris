a48b354c24aaf560b339de2619eb71fb4be3bba7
czubajka <czubajka.kania@gmail.com>
2019-09-15 17:17:03 +0200

    Added Rotate function.

--- ConsoleTetris/src/Tetris.cpp
+++ ConsoleTetris/src/Tetris.cpp
@@ -2,6 +2,18 @@
 
 std::wstring tetromino[7];
 
+int Rotate(int px, int py, int rotation)
+{
+	switch (rotation % 4)
+	{
+	case 0: return py * 4 + px;			// 0 degrees
+	case 1: return 12 + py - (px * 4);	// 90 degrees
+	case 2: return 15 - (py * 4) - px;	// 180 degrees
+	case 3: return 3 - py + (px * 4);	// 270 degrees
+	}
+	return 0;
+}
+
 int main()
 {
 	//create assets
