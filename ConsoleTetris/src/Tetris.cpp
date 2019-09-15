d7e4aa15649ef0b5c8dd1aa1895ad4cbbae4989e
czubajka <czubajka.kania@gmail.com>
2019-09-15 17:49:01 +0200

    Allocated and filled playField.

--- ConsoleTetris/src/Tetris.cpp
+++ ConsoleTetris/src/Tetris.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
 std::wstring tetromino[7];
+constexpr int fieldWidth { 12 };
+constexpr int fieldHeight { 18 };
+unsigned char *playField { nullptr };		// to be allocated dynamically
+											// unsigned char for 0 to represent white space
+
 
 int Rotate(int px, int py, int rotation)
 {
 	switch (rotation % 4)
 	{
-	case 0: return py * 4 + px;			// 0 degrees
-	case 1: return 12 + py - (px * 4);	// 90 degrees
-	case 2: return 15 - (py * 4) - px;	// 180 degrees
-	case 3: return 3 - py + (px * 4);	// 270 degrees
-	}
+	case 0: return py * 4 + px;				// 0 degrees
+	case 1: return 12 + py - (px * 4);		// 90 degrees
+	case 2: return 15 - (py * 4) - px;		// 180 degrees
+	case 3: return 3 - py + (px * 4);		// 270 degrees
+	}	
 	return 0;
 }
 
@@ -52,6 +57,10 @@ int main()
 	tetromino[6].append(L".X..");
 	tetromino[6].append(L".X..");
 
+	playField = new unsigned char[fieldHeight * fieldWidth];		// create playfield buffer
+	for(int x = 0; x < fieldWidth; x++)								// fill the board, make board boundary
+		for (int y = 0; y < fieldHeight; y++)
+			playField[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1 || y == fieldHeight - 1) ? 9 : 0;	// 9 represents border, 0 empty space
 
 	std::cout << "Console Tetris" << std::endl;
 	std::cin.get();
