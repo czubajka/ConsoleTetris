691bac33a94f3eb48c780649138cf5b4e5e9803a
czubajka <czubajka.kania@gmail.com>
2019-09-17 14:19:42 +0200

    Added game loop.

--- ConsoleTetris/src/Tetris.cpp
+++ ConsoleTetris/src/Tetris.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <windows.h>
 
-constexpr int screenWidth { 120 };
-constexpr int screenHeight { 40 };
+constexpr int screenWidth { 80 };
+constexpr int screenHeight { 30 };
 
 std::wstring tetromino[7];
 constexpr int fieldWidth { 12 };
@@ -25,16 +25,11 @@ int Rotate(int px, int py, int rotation)
 
 int main()
 {
-	// create screen buffer
-	const wchar_t *screen = new wchar_t[screenWidth * screenHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
-
-	// display frame
-	WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0,0 }, &dwBytesWritten);
-
-
+	//HWND console = GetConsoleWindow();
+	//RECT r;
+	//GetWindowRect(console, &r); //stores the console's current dimensions
+	//MoveWindow(console, r.left, r.top, screenWidth*10, screenHeight*10, TRUE); // 800 width, 100 height
+	
 	//create assets
 	tetromino[0].append(L"..X.");
 	tetromino[0].append(L"..X.");
@@ -71,11 +66,32 @@ int main()
 	tetromino[6].append(L".X..");
 	tetromino[6].append(L".X..");
 
+
+
+	// create screen buffer
+	wchar_t *screen = new wchar_t[screenWidth * screenHeight];
+	for (int i = 0; i < screenHeight * screenWidth; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+
 	playField = new unsigned char[fieldHeight * fieldWidth];		// create playfield buffer
 	for(int x = 0; x < fieldWidth; x++)								// fill the board, make board boundary
 		for (int y = 0; y < fieldHeight; y++)
-			playField[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1 || y == fieldHeight - 1) ? 9 : 0;	// 9 represents border, 0 empty space
+			playField[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1 || y == fieldHeight - 1) ? 9 : 0;	// 9 represents border, 0 empty space	
+	
+																												
+	// game loop
+	bool gameOver{ false };
+	while (!gameOver)
+	{
+		// draw field
+		for (int x = 0; x < fieldWidth; x++)
+			for (int y = 0; y < fieldHeight; y++)
+				screen[(y + 2) * screenWidth + (x + 2)] = L" ABCDEFG=#"[playField[y * fieldWidth + x]];		// +2 for drawing span
 
-	std::cout << "Console Tetris" << std::endl;
-	std::cin.get();
+		// display frame
+		WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0,0 }, &dwBytesWritten);
+	}
 }
